87106142286f4ab977daf8ebd5f58db06a87dfe9
Michael Bestas <mkbestas@lineageos.org>
2024-10-26 19:07:10 +0700

    nx606j: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc

--- extract-files.sh
+++ extract-files.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
 #
-# Copyright (C) 2018-2023 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
 function blob_fixup() {
     case "${1}" in
-    vendor/lib/hw/audio.primary.sdm845.so)
-        "${PATCHELF}" --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
-        ;;
-    vendor/lib64/hw/audio.primary.sdm845.so)
-        "${PATCHELF}" --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
-        ;;
-    vendor/lib64/libgoodixfingerprintd_binder.so)
-        "${PATCHELF}" --add-needed "libbinder_shim.so" "${2}"
-        ;;
-    vendor/lib64/libSNPE.so)
-        "${PATCHELF_0_17_2}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
-        ;;
-    vendor/bin/ultrasonicd)
-        "${PATCHELF}" --remove-needed "libmedia.so" "${2}"
-        ;;
-    vendor/lib64/vendor.goodix.hardware.fingerprintextension@1.0.so)
-        "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
-        ;;
+        vendor/lib/hw/audio.primary.sdm845.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
+            ;;
+        vendor/lib64/hw/audio.primary.sdm845.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
+            ;;
+        vendor/lib64/libgoodixfingerprintd_binder.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libbinder_shim.so" "${2}" || "${PATCHELF}" --add-needed "libbinder_shim.so" "${2}"
+            ;;
+        vendor/lib64/libSNPE.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF_0_17_2}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+            ;;
+        vendor/bin/ultrasonicd)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --remove-needed "libmedia.so" "${2}"
+            ;;
+        vendor/lib64/vendor.goodix.hardware.fingerprintextension@1.0.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
+            ;;
+        *)
+            return 1
+            ;;
     esac
+
+    return 0
 }
 
 # If we're being sourced by the common script that we called,
@@ -39,5 +50,6 @@ set -e
 export DEVICE=nx606j
 export DEVICE_COMMON=sdm845-common
 export VENDOR=nubia
+export VENDOR_COMMON=${VENDOR}
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
